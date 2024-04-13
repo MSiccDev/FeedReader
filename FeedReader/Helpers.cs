74345d7475db248ac917e57b741318e1ef084eb6
Marco Siccardi <68176309+MSicc@users.noreply.github.com>
2024-04-13 09:10:29 +0200

    work in progress, just saving the work already done

--- FeedReader/Helpers.cs
+++ FeedReader/Helpers.cs
@@ -18,6 +18,7 @@
     {
         private const string ACCEPT_HEADER_NAME = "Accept";
         private const string ACCEPT_HEADER_VALUE = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+        
         private const string USER_AGENT_NAME = "User-Agent";
         private const string USER_AGENT_VALUE = "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0";
 
